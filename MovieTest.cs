c2649ae6544487f68c4e7d43fff427c438ee67b0
AdamTasma <adamtasma@gmail.com>
2018-02-04 21:17:21 -0500

    first commit

---
+++ MovieTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    [TestFixture]
+    class MovieTest
+    {
+        [Test]
+        public void DisplayMenuWorks()
+        {
+            int expected = 1;
+            int actual = Program.displayMenu();
+            Assert.AreEqual(expected, actual, "It doesn't display the menu");
+        }
+
+    }
+}
